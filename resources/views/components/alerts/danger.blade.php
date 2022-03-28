831279c35ff54967ed3ace1b64655445136d8601
randika-chathuranga <randikachathuranga219@gmail.com>
2022-03-28 08:30:19 -0700

    changed login interface

--- resources/views/components/alerts/danger.blade.php
+++ resources/views/components/alerts/danger.blade.php
@@ -5,5 +5,5 @@
 	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 		<span aria-hidden="true">&times;</span>
 	</button>
-	<strong>Oh snap!</strong> {{$error}}
+	<strong></strong> {{$error}}
 </div>
